a1c5ddc17a051cf24f956a0b6d3ef83d3829ad43
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-01 11:12:31 -0400

    learn string js class-2

---
+++ Class-2/string.js
@@ -0,0 +1,23 @@
+var name = 'Alejandro',
+surName = 'Ortegano'
+
+var nameCl = name.toUpperCase()
+var surNameTiny = surName.toLowerCase()
+
+var firstLetterName = name.charAt(0)
+
+var namequantity = name.length
+
+var fullName = `${name} ${surName}`
+
+var str = name.substr(0, 3)
+
+var lastName = name.charAt(name.length - 1)
+
+console.log(nameCl)
+console.log(surNameTiny)
+console.log(firstLetterName)
+console.log(namequantity)
+console.log(fullName)
+console.log(str)
+console.log(lastName)
