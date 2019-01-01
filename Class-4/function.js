69c592241cc92c64698297dddaea8facfe2839bd
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-01 12:40:46 -0400

    learn fucntion on js class-4

---
+++ Class-4/function.js
@@ -0,0 +1,12 @@
+var name = 'Alejandro',
+age = 28
+
+function PrintAge(_name, _age) {
+  console.log(`${_name} is ${_age} years old`);
+
+}
+
+PrintAge(name, age)
+PrintAge('Anthony', 23)
+PrintAge('Pedro', 40)
+PrintAge('Javier', 27)
