c737d1cc9763ef4009d4a097e8186a52a7bb85eb
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-01 14:53:10 -0400

    Destruct objects class-7

---
+++ Class-7/object.js
@@ -0,0 +1,24 @@
+// object
+// key: value
+
+var alejandro = {
+  name: 'Alejandro',
+  surName: 'Ortegano',
+  age: '23'
+}
+
+var anthony = {
+  name : 'Anthony',
+  surName: 'Sanoja',
+  age: 23
+}
+
+function PrintNameCl(people) {
+  // var name = people.name
+  var { name } = people
+  console.log(name.toUpperCase());
+}
+
+PrintNameCl(alejandro)
+PrintNameCl(anthony)
+PrintNameCl({ name: 'Javier' })
