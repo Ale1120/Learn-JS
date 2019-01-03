34b5d7903ffd5b2fa7fd5ee13e650e1a273f6e7c
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-03 14:32:46 -0400

    learn propotype and this js class-21

---
+++ Class-21/propotype.js
@@ -0,0 +1,20 @@
+// prototype === Class
+
+function People (name, surName, ) {
+  this.name = name
+  this.surName = surName
+}
+
+People.prototype.greet = function () {
+  console.log(`hello, my name is ${this.name} ${this.surName} `)
+
+}
+
+// create new object, prototype
+var alejandro = new People('Alejandro', 'Ortegano')
+var anthony = new People('Anthony', 'Sanoja')
+var javier = new People('Javier','Torrealba' )
+
+alejandro.greet()
+anthony.greet()
+javier.greet()
