7962c5f90a0fae0c0937cfbccc83bd67fc08828a
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-03 15:30:22 -0400

    The context of the functions: who is this  class-23 js

---
+++ Class-23/prototype.js
@@ -0,0 +1,25 @@
+// prototype === Class
+
+function People (name, surName, height) {
+  this.name = name
+  this.surName = surName
+  this.height = height
+}
+
+People.prototype.greet = function () {
+  console.log(`hello, my name is ${this.name} ${this.surName} `)
+
+}
+
+People.prototype.isTall = function () {  
+  return this.height >= 1.80
+}
+
+// create new object, prototype
+var alejandro = new People('Alejandro', 'Ortegano', 1.70)
+var anthony = new People('Anthony', 'Sanoja', 1.75)
+var javier = new People('Javier','Torrealba', 1.80 )
+
+alejandro.isTall()
+anthony.isTall()
+javier.isTall()
