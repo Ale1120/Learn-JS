72401f9df6c44f21c362cd00447d31d0ead7543b
Ale1120 <alejandro.ortegano11@gmail.com>
2019-01-03 12:18:15 -0400

    chanllege arrays class-18 js

---
+++ Class-18/chanllenge-class18.js
@@ -0,0 +1,45 @@
+var alejandro = {
+  name: 'Alejandro',
+  surName: 'Ortegano',
+  age: 23,
+  height: 1.70
+}
+
+var anthony = {
+  name : 'Anthony',
+  surName: 'Sanoja',
+  age: 23,
+  height: 1.75
+}
+
+var richi = {
+  name : 'Richeidy',
+  surName: 'Salcedo',
+  age: 21,
+  height: 1.73
+}
+
+var javier = {
+  name : 'Javier',
+  surName: 'Torrealba',
+  age: 28,
+  height: 1.78
+}
+
+var piter = {
+  name : 'Pedro',
+  surName: 'Alvarado',
+  age: 40,
+  height: 1.85
+}
+
+const isTall = people => people.height > 1.8
+const isSmall = people => !isTall(people.height)
+
+var people = [alejandro, anthony, richi, javier, piter]
+
+// new array
+var tallPeople = people.filter(isTall)
+var smallPeople = people.filter(isSmall)
+
+console.log(smallPeople);
